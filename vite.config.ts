7c1cb1014e3a1150c8d411e3ed8dacb5b9d85276
KraXen72 <dpelech1@gmail.com>
2025-10-13 22:35:27 +0200

    fix wasm

--- vite.config.ts
+++ vite.config.ts
@@ -7,8 +7,16 @@ export default defineConfig({
   plugins: [devtools(), solidPlugin(), tailwindcss()],
   server: {
     port: 3000,
+    headers: {
+      'Cross-Origin-Embedder-Policy': 'require-corp',
+      'Cross-Origin-Opener-Policy': 'same-origin',
+    },
   },
   build: {
     target: 'esnext',
   },
+  optimizeDeps: {
+    exclude: ['harper.js'],
+  },
+  assetsInclude: ['**/*.wasm'],
 });
